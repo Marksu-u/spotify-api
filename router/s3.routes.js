92ab053312adff81e74bbccb0fee703f22d76794
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2024-01-20 17:38:32 +0100

    remove unused code

--- router/s3.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import {getAWSaudio} from '../controllers/s3.controller.js';
-
-const router = express.Router();
-
-router.get('/list-s3-objects', getAWSaudio);
-
-export default router;
